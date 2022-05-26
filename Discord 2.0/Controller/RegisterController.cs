d8406f83274c48464c1e9853e7b2776ae0ae2c54
it.pgee.burgas <it.pgee.burgas@gmail.com>
2022-05-26 13:28:37 +0300

    Added DB Login and Register

---
+++ Discord 2.0/Controller/RegisterController.cs
@@ -0,0 +1,52 @@
+﻿using Discord_2._0.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_2._0.Controller
+{
+    class RegisterController
+    {
+        public void AddUser(Users s)
+        {
+            using (CommunicationSoftwareDBEntities ex = new CommunicationSoftwareDBEntities())
+            {
+                var lastUser = ex.Users.ToList().LastOrDefault();
+                if (lastUser == null)
+                {
+                    lastUser = new Users();
+                    lastUser.Id = 0;
+                }
+
+                s.Id = lastUser.Id + 1;
+                ex.Users.Add(s);
+                ex.SaveChanges();
+            }
+        }
+
+        public string Login(string email, string pass)
+        {
+            using (CommunicationSoftwareDBEntities cs = new CommunicationSoftwareDBEntities())
+            {
+                var Login = cs.Users.Where(s => s.Email == email).FirstOrDefault();
+                if (Login != null)
+                {
+                    if (Login.Email == email && Login.Password == pass)
+                    {
+                        return null;
+                    }
+                    else
+                    {
+                        return "The Username or password is incorect, please try again";
+                    }
+                }
+                else
+                {
+                    return "No suck user!";
+                }
+            }
+        }
+    }
+}
